d51700d3e1d83369d9ad52170eb0852e4510fd81
Elodie Wu <elodie.wu@outlook.com>
2026-07-03 14:55:00 +1200

    feat: add ProductList.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,31 @@
 import styles from "./Product.module.css";
 import styled from "styled-components";
 
+const StyledProductTextContainer = styled.div`
+  position: absolute;
+  top: 1.5em;
+  padding-left: 1.5em;
+  padding-top: 1.5em;
+  color: ${(props) => props.$textColor || "white"};
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+`;
+
 const StyledProductContainer = styled.div`
-  maxWidth: 28rem;
+  max-width: 28rem;
   position: relative;
-  transition: transform ${(props) => props.transition || "0.1s"} ease-in-out;
+  transition: transform ${(props) => props.$transition || "0.1s"} ease-in-out;
 
   &:hover {
-    transform: scale(${(props) => props.scale || 1.05});
+    transform: scale(${(props) => props.$scale || 1.05});
     cursor: pointer;
+  }
 `;
 
 function Product({
   image,
   title,
   detail,
+  textColor,
   scale = 1.05,
   transition = "0.5s",
   onProductClick,
@@ -26,15 +37,15 @@ function Product({
   };
   return (
     <StyledProductContainer
-      scale={scale}
-      transition={transition}
-      onClick={() => onProductClick(title)}
+      $scale={scale}
+      $transition={transition}
+      onClick={() => onProductClick?.(title)}
     >
       <img style={imgStyle} src={image} alt={title} />
-      <div className={styles.productTextContainer}>
+      <StyledProductTextContainer $textColor={textColor}>
         <div className={styles.productTitle}>{title}</div>
         <div className={styles["product-detail"]}>{detail}</div>
-      </div>
+      </StyledProductTextContainer>
     </StyledProductContainer>
   );
 }
