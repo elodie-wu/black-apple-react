53043cd70de89671a3abb253a485c0ea8ff61481
Elodie Wu <elodie.wu@outlook.com>
2026-07-03 16:12:50 +1200

    refactor: split ProductList into smaller components

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 import Production from "@components/Product";
 
+const ReleaseNote = () => (
+  <div>
+    <h2>Please wait</h2>
+    <h1>New product release date: January 1, 2199</h1>
+  </div>
+);
+const ProductNotFound = () => (
+  <div>
+    <h1>Product not released</h1>
+    <h2>Please wait</h2>
+  </div>
+);
+const ListTitle = () => (
+  <div style={{ display: "flex", justifyContent: "center" }}>
+    <h1
+      style={{
+        fontWeight: "800",
+        backgroundPosition: "center",
+        backgroundImage: "url('src/assets/lines.png')",
+      }}
+    >
+      New arrivals. New possibilities.
+    </h1>
+  </div>
+);
 function ProductList({ data }) {
   const handleProductClick = (title) => {
     alert(`Product clicked: ${title}`); // 点击产品时弹出提示框 alert box pops up when product is clicked
   };
 
-  const isReleased = new Date() <= new Date("2199-09-01");
+  const isReleased = new Date() <= new Date("2199-01-01");
 
   if (!isReleased) {
-    return <div>Product not released yet. Please check back later.</div>;
+    return <ReleaseNote />;
   }
   if (data.length <= 0) {
-    return <div>No products available.</div>;
+    return <ProductNotFound />;
   }
   return (
     <div
@@ -22,17 +47,7 @@ function ProductList({ data }) {
         rowGap: "3rem",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1
-          style={{
-            fontWeight: "800",
-            backgroundImage: "url('src/assets/lines.png')",
-            backgroundPosition: "center",
-          }}
-        >
-          New arrivals. New possibilities.
-        </h1>
-      </div>
+      <ListTitle />
       {data.map((p) => (
         <Production {...p} key={p.title} onProductClick={handleProductClick} />
       ))}
