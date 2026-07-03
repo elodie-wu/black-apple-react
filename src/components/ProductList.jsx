c3f80c63d6d5ccd01a6dad54762751957eac6683
Elodie Wu <elodie.wu@outlook.com>
2026-07-03 15:26:17 +1200

    refactor: simplify conditional rendering with ternary operator

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,9 +5,7 @@ function ProductList({ data }) {
   const handleProductClick = (title) => {
     alert(`Product clicked: ${title}`); // 点击产品时弹出提示框 alert box pops up when product is clicked
   };
-  const isReleased = new Date() >= new Date("2199-09-01");
-
-  return data.length >0 && (
+  return data.length > 0 ? (
     <div
       style={{
         display: "grid",
@@ -29,7 +27,9 @@ function ProductList({ data }) {
       {data.map((p) => (
         <Production {...p} key={p.title} onProductClick={handleProductClick} />
       ))}
-    </div>,
+    </div>
+  ) : (
+    <div>No products available.</div>
   );
 }
 
